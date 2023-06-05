0037665d0836a7c89d9f06c98a70ce8cda597ec5
Tairovgg27 <99729976+Tairovgg27@users.noreply.github.com>
2023-06-05 07:00:28 +0300

    Add files via upload

---
+++ 5labs/main.py
@@ -0,0 +1,21 @@
+print("test")
+for i in range(0, 25, 5):
+	print(i ** (i + 1))
+
+
+if(19 > 10 - 12):
+	print("yes")
+else:
+	print("hey")
+	print("No")
+
+print(9000 if False else 10)
+
+
+#some change
+
+print("i need freedom and change this file")
+
+print("hellow i am from test2 branch and i will be merged with main with troubles")
+
+print("hello i am in new branch test1")
